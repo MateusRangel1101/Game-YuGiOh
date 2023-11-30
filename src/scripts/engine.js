d24b22580b25cdc7677b9f15b82188f79d5a9a14
Mateus Rangel <mateusrangel.com.br@gmail.com>
2023-11-30 18:20:30 -0300

    iniciando a parte JS

---
+++ src/scripts/engine.js
@@ -0,0 +1,29 @@
+const state = {
+    score: {
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById('socre_points')
+    },
+    cardSprites: {
+        avatar: document.getElementById('card-image'),
+        name: document.getElementById('card-name'),
+        type: document.getElementById('card-type'),
+    },
+    fieldCards: {
+        player: getElementById('player-field-card'),
+        computer: getElementById('computer-field-card'),
+    },
+    actions: {
+        button: getElementById('next-duel')
+    },
+}
+
+// const players = {
+//     player1: 'player-cards'
+// }
+
+function init() {
+
+}
+
+init()
